bf7bf2ec9b337cfa11827a86fd4450402cead528
HectorOmar5 <omar.gonzalez.hong@gmail.com>
2020-10-09 17:41:29 -0500

    Se creo la clase Alumno, se enlazó al controlador ya la vista.

---
+++ ASPCore-App/ASPCore-App/Controllers/AlumnoModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore_App.Controllers
+{
+    public class AlumnoModel
+    { 
+        public string Nombre { set; get; }
+        public string App { set; get; }
+        public string Apm { set; get; }
+        public string NC { set; get; }
+        public string Semestre { set; get; }
+        public string Carrera { set; get; }
+        public string Promedio { set; get; }
+        public string FechaIngreso { set; get; }
+        /////////////////////////////////////////
+        public string Nombre_2 { set; get; }
+        public string App_2 { set; get; }
+        public string Apm_2 { set; get; }
+        public string NC_2 { set; get; }
+        public string Semestre_2 { set; get; }
+        public string Carrera_2 { set; get; }
+        public string Promedio_2 { set; get; }
+        public string FechaIngreso_2 { set; get; }
+        //////////////////////////////////////////
+        public string Nombre_3 { set; get; }
+        public string App_3 { set; get; }
+        public string Apm_3 { set; get; }
+        public string NC_3 { set; get; }
+        public string Semestre_3 { set; get; }
+        public string Carrera_3 { set; get; }
+        public string Promedio_3 { set; get; }
+        public string FechaIngreso_3 { set; get; }
+        ///////////////////////////////////////////
+        public string Nombre_4 { set; get; }
+        public string App_4 { set; get; }
+        public string Apm_4 { set; get; }
+        public string NC_4 { set; get; }
+        public string Semestre_4 { set; get; }
+        public string Carrera_4 { set; get; }
+        public string Promedio_4 { set; get; }
+        public string FechaIngreso_4 { set; get; }
+    }
+}
