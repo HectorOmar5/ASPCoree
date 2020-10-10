f8cd0a7198210e28f290763e52d6762706aec6dc
HectorOmar5 <omar.gonzalez.hong@gmail.com>
2020-10-09 19:41:50 -0500

    Se creo la clase Alumno, se enlazó al controlador y a la vista

--- ASPCore-App/ASPCore-App/Controllers/AlumnoModel.cs
+++ ASPCore-App/ASPCore-App/Controllers/AlumnoModel.cs
@@ -7,40 +7,36 @@ namespace ASPCore_App.Controllers
 {
     public class AlumnoModel
     { 
-        public string Nombre { set; get; }
-        public string App { set; get; }
-        public string Apm { set; get; }
-        public string NC { set; get; }
-        public string Semestre { set; get; }
-        public string Carrera { set; get; }
-        public string Promedio { set; get; }
-        public string FechaIngreso { set; get; }
+        public string Nombre { get; set; }
+        public string App { get; set; }
+        public string Apm { get; set; }
+        public string NC { get; set; }
+        public int Semestre { get; set; }
+        public string Carrera { get; set; }
+        public double Promedio { get; set; }
         /////////////////////////////////////////
-        public string Nombre_2 { set; get; }
-        public string App_2 { set; get; }
-        public string Apm_2 { set; get; }
-        public string NC_2 { set; get; }
-        public string Semestre_2 { set; get; }
-        public string Carrera_2 { set; get; }
-        public string Promedio_2 { set; get; }
-        public string FechaIngreso_2 { set; get; }
+        public string Nombre_2 { get; set; }
+        public string App_2 { get; set; }
+        public string Apm_2 { get; set; }
+        public string NC_2 { get; set; }
+        public int Semestre_2 { get; set; }
+        public string Carrera_2 { get; set; }
+        public double Promedio_2 { get; set; }
         //////////////////////////////////////////
-        public string Nombre_3 { set; get; }
-        public string App_3 { set; get; }
-        public string Apm_3 { set; get; }
-        public string NC_3 { set; get; }
-        public string Semestre_3 { set; get; }
-        public string Carrera_3 { set; get; }
-        public string Promedio_3 { set; get; }
-        public string FechaIngreso_3 { set; get; }
+        public string Nombre_3 { get; set; }
+        public string App_3 { get; set; }
+        public string Apm_3 { get; set; }
+        public string NC_3 { get; set; }
+        public int Semestre_3 { get; set; }
+        public string Carrera_3 { get; set; }
+        public double Promedio_3 { get; set; }
         ///////////////////////////////////////////
-        public string Nombre_4 { set; get; }
-        public string App_4 { set; get; }
-        public string Apm_4 { set; get; }
-        public string NC_4 { set; get; }
-        public string Semestre_4 { set; get; }
-        public string Carrera_4 { set; get; }
-        public string Promedio_4 { set; get; }
-        public string FechaIngreso_4 { set; get; }
+        public string Nombre_4 { get; set; }
+        public string App_4 { get; set; }
+        public string Apm_4 { get; set; }
+        public string NC_4 { get; set; }
+        public int Semestre_4 { get; set; }
+        public string Carrera_4 { get; set; }
+        public double Promedio_4 { get; set; }
     }
 }
--- ASPCore-App/ASPCore-App/Controllers/HomeController.cs
+++ ASPCore-App/ASPCore-App/Controllers/HomeController.cs
@@ -12,7 +12,36 @@ namespace ASPCore_App.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var a = new AlumnoModel();
+            a.Nombre = "Omar";
+            a.App = "Nava";
+            a.Apm = "Gonzalez";
+            a.Carrera = "Informatica";
+            a.NC = "17030098";
+            a.Promedio = "90";
+            /////////////////////////
+            a.Nombre_2 = "Saul";
+            a.App_2 = "Burciaga";
+            a.Apm_2 = "Hernandez";
+            a.Carrera_2 = "Informatica";
+            a.NC_2 = "17030078";
+            a.Promedio_2 = "95";
+            ///////////////////////////
+            a.Nombre_3 = "Luis";
+            a.App_3 = "Mendieta";
+            a.Apm_3 = "Hernandez";
+            a.Carrera_3 = "Informatica";
+            a.NC_3 = "17030065";
+            a.Promedio_3 = "88";
+            //////////////////////////
+            a.Nombre_4 = "J. Carlos";
+            a.App_4 = "Magallanes";
+            a.Apm_4 = "Orona";
+            a.Carrera_4 = "Informatica";
+            a.NC_4 = "17030075";
+            a.Promedio_4 = "83";
+
+            return View(a);
         }
 
         public IActionResult Privacy()
